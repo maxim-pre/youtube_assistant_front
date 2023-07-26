063bf028c6cf6f5c66e2e307282913aedf5c39b5
Maxim Prestwich <maximprestwich1@gmail.com>
2023-07-26 10:58:42 +0100

    add poduction api url to config file

--- src/apiRoute.js
+++ src/apiRoute.js
@@ -1,8 +1,9 @@
 let apiRoute;
 const port = 8000;
+
 const apiUrls = {
   development: `http://localhost:${port}/api/`,
-  production: `https://frozen-caverns-09464.herokuapp.com/`,
+  production: `https://youtubeassistant-371ce8d1642b.herokuapp.com/api/`,
 };
 if (window.location.hostname === "localhost") {
   apiRoute = apiUrls.development;
